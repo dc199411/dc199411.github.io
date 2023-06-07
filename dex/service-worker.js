40b99144bbacd3f2487c89de9f851cc2d97eca3f
dc199411 <davincorbett7@gmail.com>
2023-06-07 10:47:23 -0400

    token list logo's fixes

--- dex/service-worker.js
+++ dex/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b6efd18fecee07f2a1dde4f62fa6779c.js"
+  "/dex/precache-manifest.1d0fbc6078f24fbd69e74f4e1a90120a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
